8310e19cb81ab699f890a32efaa01fd2ed9c88de
Eomaxl <souravsnigdh@gmail.com>
2023-12-11 18:36:05 +0530

    Sidenav and topHeader implementation completed

---
+++ app/(dashboard)/(routes)/_components/TopHeader.jsx
@@ -0,0 +1,16 @@
+import { UserButton } from '@clerk/nextjs';
+import { AlignJustify } from 'lucide-react';
+import React from 'react';
+import Image from 'next/image';
+
+const TopHeader = () => {
+    return (
+        <div className='flex p-5 border-b items-center justify-between md:justify-end'>
+            <AlignJustify className='md:hidden'/>
+            <Image src='/logoipsum-297.svg' width={150} height={100} className='md:hidden'/>
+            <UserButton/>
+        </div>
+    )
+}
+
+export default TopHeader
